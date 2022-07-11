41e91aacc6ab6be851df86e57844ec5bd5733e10
Vladimir Pavlov <bob.pavlov.a7@gmail.com>
2022-07-11 14:02:14 +0300

    Created Product entity.

---
+++ SupermarketManagement.Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketManagement.Entities;
+public class Product
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    [MinLength(2)]
+    public string Name { get; set; } = null!;
+    [Required]
+    public int CategoryId { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+}
