8764640689c59a9f1c694d0beed41024bf86403f
Pavlov-Vladimir <bob.pavlov.a7@gmail.com>
2022-08-13 12:57:46 +0300

    Created tests project.

---
+++ SupermarketManagement.Tests/Common/MarketContextFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace SupermarketManagement.Tests.Common;
+public class MarketContextFactory
+{
+    public static MarketDbContext Create()
+    {
+        var options = new DbContextOptionsBuilder<MarketDbContext>()
+            .UseInMemoryDatabase("DB_FOR_TESTS")
+            .Options;
+        return new MarketDbContext(options);
+    }
+
+    public static void Destroy(MarketDbContext dbContext)
+    {
+        dbContext.Database.EnsureDeleted();
+        dbContext.Dispose();
+    }
+}
